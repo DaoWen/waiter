f2acfa74e66bd4da0560448266ba4da3acb14516
Nick Vrvilo <Nick.Vrvilo@alumni.rice.edu>
2018-05-17 08:59:51 -0500

    json-count-only

--- waiter/src/waiter/util/utils.clj
+++ waiter/src/waiter/util/utils.clj
@@ -118,7 +118,7 @@
 (defmacro json-counted-vector
   "Return a vector of the arguments that will be rendered as an item count in a json response."
   [& xs]
-  `^:count-only [~@xs])
+  `^:json-count-only [~@xs])
 
 (defn stringify-elements
   [k v]
