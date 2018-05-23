132e81a41fbddf39155c4654165972dccc22fc31
Nick Vrvilo <Nick.Vrvilo@alumni.rice.edu>
2018-05-23 14:53:43 -0500

    add missing timer_context.clj

---
+++ waiter/src/waiter/util/timer_context.clj
@@ -0,0 +1,25 @@
+;;
+;;       Copyright (c) 2017 Two Sigma Investments, LP.
+;;       All Rights Reserved
+;;
+;;       THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF
+;;       Two Sigma Investments, LP.
+;;
+;;       The copyright notice above does not evidence any
+;;       actual or intended publication of such source code.
+;;
+(ns waiter.util.timer-context
+  (:require [clj-time.core :as t]
+            [waiter.util.date-utils :as du])
+  (:import (com.codahale.metrics Timer)
+           (java.util.concurrent TimeUnit)
+           (org.joda.time DateTime)))
+
+(defrecord TimerContext [timer start-time])
+
+(defn report-duration
+  "Report elapsed duration on the given timer metric."
+  [{:keys [timer start-time]} end-time]
+  (.update ^Timer timer
+           (t/in-millis (t/interval start-time end-time))
+           TimeUnit/MILLISECONDS))
